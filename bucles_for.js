21e7803db26df5e4f952a3318adffdafe966a1a4
Thiago Castelli <thiagocastelli02@gmail.com>
2023-02-01 17:37:14 -0300

    Avanzamos con los bucles y terminamos con el While

--- bucles_for.js
+++ bucles_for.js
@@ -1,6 +1,16 @@
 var suma = 0
 
+//for(var i=0 ; i<10 ; i++){
+//    suma = suma +1;
+//    console.log(suma);
+//};
+
+// Ahora vamos a hacer lo mismo PERO a la variable suma en vez de incrementar de a 1, se le sume la variable de iteración 
+// (osea 'i').Cambiamos el console log de suma por 'i' [y le agregamos un texto bonito]. 
+// Finalmente, también hacemos un console.log de 'suma' para ver el total.
+
 for(var i=0 ; i<10 ; i++){
-    suma = suma +1;
-    console.log(suma);
-};
+    suma = suma + i;
+    console.log('La variable de iteración: ', i );
+}
+console.log('La variable suma: ', suma)
