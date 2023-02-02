bd988742a2c7696c370673d5d869030e4a6e8236
Thiago Castelli <thiagocastelli02@gmail.com>
2023-02-02 20:40:41 -0300

    arrays con todos los metodos, sin los bucles en los arrays

--- arrays.js
+++ arrays.js
@@ -35,3 +35,43 @@ var mayoresA5 = numeros.every((num) => {
 });
 
 console.log(mayoresA5);
+
+// Split
+
+var palabra = 'Henri';
+var palabraSeparada = palabra.split('');
+console.log(palabraSeparada);
+
+// Arreglar la palabra mal escrita 'Henri' por 'Henry' con los otros métodos aprendidos, pop y push.
+
+var palabra = 'Henri';
+var palabraSeparada = palabra.split('');
+palabraSeparada.pop();
+palabraSeparada.push('y');
+console.log(palabraSeparada);
+
+
+// Join
+
+var palabraArreglada = palabraSeparada.join('');
+console.log(palabraArreglada);
+
+// ForEach
+
+var numeros = [1, 2, 3, 4, 37, 43];
+numeros.forEach((num) => console.log(num));
+
+numeros.forEach((num) => { //por cada número
+    if(num === 3) { //siempre que el número tenga 3
+        console.log(num); //traeme el número
+    } 
+});
+
+// Map
+
+var masUno = numeros.map((num) => { //acá no hay especificación, entonces
+    return num + 1; //este return aplica a todos los numeros, no solamente los que tengan 3.
+});
+console.log(masUno);
+
+
